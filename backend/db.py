c46d4b69cdba203bae7b565a109282ba8d16a760
rajyadav38 <rajyadav5566788@gmail.com>
2026-02-17 19:38:52 +0530

    Use Railway MySQL

--- backend/db.py
+++ backend/db.py
@@ -1,14 +1,13 @@
 import mysql.connector
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 conn = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
     user=os.getenv("DB_USER"),
     password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME")
+    database=os.getenv("DB_NAME"),
+    port=os.getenv("DB_PORT"),
+    ssl_disabled=False
 )
 
 cursor = conn.cursor(dictionary=True)
